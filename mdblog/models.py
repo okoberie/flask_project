cfff53c80d6794f047a09ce36608447440268c1d
okoberie <okoberie@protonmail.com>
2022-06-20 11:47:49 +0000

    adds hashed passwords and password change

--- mdblog/models.py
+++ mdblog/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 db = SQLAlchemy()
 
@@ -10,4 +12,10 @@ class Article(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
-    username = db.Column(db.String)
+    password = db.Column(db.String)
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password,password)
